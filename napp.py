afd34be7068668ada2a72f81578edaa3bd9c646e
Drishya Uniyal <54202043+uniyald823@users.noreply.github.com>
2022-02-02 22:40:30 +0530

    Update napp.py

--- napp.py
+++ napp.py
@@ -1,144 +1,101 @@
-import dash_core_components as dcc
-import dash_html_components as html
-import pandas as pd
 import dash
-import dash_bootstrap_components as dbc
-from dash.dependencies import Input, Output
-from app import app
+import dash_core_components as dcc
+import dash_html_components as html 
+from dash.dependencies import Input, Output 
 
-from plotly import graph_objs as go
-import plotly.express as px
-df = pd.read_csv("C:/Users/Drishya/list_to_be_checked.csv")
-# Iris bar figure
-'''def drawFigure():
-    return  html.Div([
-        dbc.Card(
-            dbc.CardBody([
-                dcc.Graph(
-                    figure=px.bar(
-                        df, x="sepal_width", y="sepal_length", color="species"
-                    ).update_layout(
-                        template='plotly_dark',
-                        plot_bgcolor= 'rgba(0, 0, 0, 0)',
-                        paper_bgcolor= 'rgba(0, 0, 0, 0)',
-                    ),
-                    config={
-                        'displayModeBar': False
-                    }
-                ) 
-            ])
-        ),  
-    ])'''
+import requests 
+import plotly.graph_objects as go
+import pandas as pd
 
-def drawFigure():
-    return    html.Div(
-                children=[
- #Description below the header
-        dcc.Dropdown(id='BM-dropdown',
-                options=[{'label': x, 'value': x}
-                        for x in df['Location'].unique()],
-                 value='Location',
-                 multi=False, clearable=True),
-    dcc.Graph(id='bar-chart')]
-             
-)
+pd.set_option("display.max_rows", 999)
+pd.set_option("display.max_columns", 999)
 
-def drawFigure1():
-    return  html.Div(
-            children=[
-                html.Div(children=[
-                    dcc.Dropdown(id='dropdown1',
-                              options=[{'value':x,'label':x} 
-                                       for x in df.Username.unique()],
-                              clearable=False,
-                              value='Bangkok',
-                              ),
-                    ], className='menu-l'
-                    ),
-                dcc.Graph(id='interaction2',
-                          config={'displayModeBar':False},
-                          className='card')]                
-            )
+df = pd.read_csv('WHS8_110.csv')
 
-# Text field
-def drawText():
-    return html.Div([
-        dbc.Card(
-            dbc.CardBody([
-                html.Div([
-                    html.H2("Mapping Online Hate Speech to Offline Crimes"),
-                ], style={'textAlign': 'center'}) 
-            ])
-        ),
-    ])
+new_columns = {
+    'Country' : 'country'
+}
 
+df.columns = df.iloc[0].values
+df = df[1:]
+df = df.fillna(0).copy()
+df.columns = df.columns.astype(str)
+df.columns = [col.replace('.0','') for col in df.columns]
+df.rename(columns=new_columns, inplace=True)
 
-# Build App
-app = dash.Dash(external_stylesheets=[dbc.themes.SLATE])
+#df_country = df.head(30)
+#df_year = df.head(30)
+
+app = dash.Dash() 
 
 server = app.server
 
-app.layout = html.Div([
-    dbc.Card(
-        dbc.CardBody([
-            dbc.Row([dbc.Col([drawText()], width=13),
-            ], align='center'),
-            
-            html.Br(),
-            dbc.Row([
-            dbc.Col([drawFigure()], width=5),
-            dbc.Col([drawFigure1()], width=3)
-            #dbc.Col([drawFigure()], width=6),
-            ], align='center'),
-            
-            #html.Br(),
-            #dbc.Row([dbc.Col([drawFigure()], width=9),
-            #dbc.Col([drawFigure()], width=3),
-           # ])
-            
-    ])
-    )
-])
-
-@app.callback(
-    Output(component_id="bar-chart", component_property="figure"),
-    [Input(component_id="BM-dropdown", component_property="value")],
-)
-        
-def display_BM_composition(selected_BM):
-    filtered_BM = df[df.Username == selected_BM]  # Only use unique values in column "BM_NAME" selected in dropdown
-
-    barchart = px.line(
-        data_frame=filtered_BM,
-        x="Label").update_layout(
-                        template='plotly_dark',
-                        plot_bgcolor= 'rgba(0, 0, 0, 0)',
-                        paper_bgcolor= 'rgba(0, 0, 0, 0)',
-                    )
-    return barchart
-
-
-@app.callback(
- Output('interaction2', 'figure'),
-    [Input('dropdown1','value')]
+app.layout = html.Div(
+    [ 
+        html.H1(
+            'Immunization Dashboard',
+            style = {'text-align' : 'center'}
+        ), 
+        html.Div([ 
+            dcc.Dropdown(
+                id='year_selection',
+                options=[
+                    {'label': '2019', 'value': '2019'},
+                    {'label': '2018', 'value': '2018'},
+                    {'label': '2017', 'value': '2017'},
+                    {'label': '2016', 'value': '2016'},
+                    {'label': '2015', 'value': '2015'},
+                    {'label': '2014', 'value': '2014'},
+                    {'label': '2013', 'value': '2013'},
+                    {'label': '2012', 'value': '2012'}
+                ],
+                value = '2019'
+            ),
+            dcc.Graph(id ='bargraph'),
+            dcc.Dropdown(
+                id='country_selection',
+                options=[
+                    {'label': 'Romania', 'value': 'Romania'},
+                    {'label': 'United States of America', 'value': 'United States of America'},
+                    {'label': 'France', 'value': 'France'},
+                    {'label': 'Hungary', 'value': 'Hungary'},
+                    {'label': 'Italy', 'value': 'Italy'},
+                    {'label': 'Sudan', 'value': 'Sudan'},
+                    {'label': 'Spain', 'value': 'Spain'},
+                    {'label': 'Egypt', 'value': 'Egypt'},
+                    {'label': 'India', 'value': 'India'},
+                ],
+                value = 'Romania'
+            ),
+            dcc.Graph(id ='linegraph'),
+        ],
+    style= {'padding':10})
+    ]
 )
 
+@app.callback(
+    Output('bargraph','figure'),         
+    [Input('year_selection','value')]
+)
+def retrieve_revenue(year):
+    df_country = df['country'].head(20)
+    df_year = df[year].head(20)
+    datapoints = {'data': [go.Bar(x=df_country, y=df_year)],'layout': dict(yaxis={'title':'Vaccination %'}, )} 
+    return datapoints
+ 
+@app.callback(
+    Output('linegraph','figure'), 
+    [Input('country_selection','value')]
+) 
+def retrieve_revenue(country): 
+    df_country = df[df['country'] == country]
+    df_country = df_country.T.drop('country', axis=0)
+    df_country.columns = ['rate']
+    df_country = df_country.rename(columns={df_country.columns[0] : 'rate'})
+    years = list(df_country.index)
+    rates = list(df_country['rate'].values)
+    datapoints = {'data': [go.Scatter(x=years, y=rates, mode="lines+markers")], 'layout' : dict(yaxis=dict(range=[0,100]))}
+    return datapoints 
 
-def update_pie_chart(select_d1):
-    df3=df.loc[df['Username']==select_d1]
-    ## using dash to make the pie chart
-    fig1=go.Figure(data=[go.Pie(labels=df3['Label'].value_counts().index.tolist(),
-                         values=list(df3['Label'].value_counts()))])
-    ## customizing the title of the pie chart
-    #names={'sex':'Sex','risk':'Case Origin'}
-    
-    fig1.update_layout(template='plotly_dark',
-                        plot_bgcolor= 'rgba(0, 0, 0, 0)',
-                        paper_bgcolor= 'rgba(0, 0, 0, 0)',)
-    return fig1 #to be outputted!
-
-
-
-
-# Run app and display result inline in the notebook
-app.run_server(debug=True)
+if __name__ == '__main__': 
+    app.run_server(debug=True)
